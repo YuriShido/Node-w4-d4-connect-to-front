d35510c00f856d7e385fe9d84549d55f8bdceb4a
YuriShido <sydonce57@gmail.com>
2021-05-21 01:44:11 -0700

    add front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Axios from 'axios'
+
+import './App.css'
+
+import Home from './pages/Home'
+import Login from './components/auth/Login'
+import UserContext from './context/userContext'
 
 function App() {
+  const [userData, setUserData] = useState()
+  useEffect(() => {
+    const showUsers = async () => {
+      const users = await Axios.get("http://localhost:8080/users")
+      setUserData(users)
+    }
+    showUsers()
+  }, [])
+
+  useEffect(() => {
+    console.log(userData)
+  }, [userData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </UserContext.Provider>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
